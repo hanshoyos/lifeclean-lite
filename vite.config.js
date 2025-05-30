eeb2aedc0e3fcbe059e525a4c7e7c93d3ded299f
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2025-05-30 13:20:59 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/lifeclean-lite/', // 👈 This is the fix
   plugins: [react()],
 });

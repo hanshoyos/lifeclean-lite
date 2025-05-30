d5f15baf9da9ca3142587af2c0c605c70ad2b593
HansTechHub <160248751+hanshoyos@users.noreply.github.com>
2025-05-30 13:07:58 -0400

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        hacker: '#00FF9C',
+        dark: '#0F0F0F',
+      },
+    },
+  },
+  plugins: [],
+};
